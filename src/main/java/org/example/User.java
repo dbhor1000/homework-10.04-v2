df363f61f325cde107f8dd0cbd73a46868e6e483
dbhor <dbhor1000@gmail.com>
2023-04-11 21:50:53 +0300

    initial commit to master

---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,33 @@
+package org.example;
+
+public class User {
+
+    private String login;
+    private String email;
+
+    public User() {
+    }
+
+    public User(String login, String email) {
+        this.login = login;
+        this.email = email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
+
+
---
+++ src/test/java/org/example/UserTestConstants.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class UserTestConstants {
+    public static final String DEFAULT_LOGIN = "Default";
+    public static final String DEFAULT_EMAIL = "default@mail.def";
+    public static final String INCORRECT_EMAIL = "defaultmail.def";
+    public static final String DEFAULT_LOGIN_AS_MAIL = "default@mail.def";
+}
